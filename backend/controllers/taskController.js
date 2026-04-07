817d85fcd34945b3adb910461cffda36fff256f1
ADJs Codes <adjscode@gmail.com>
2026-04-07 15:09:36 +0100

    First commit

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,140 @@
+const Task = require("../models/Task");
+
+//@desc Get all tasks (Admin: all, user: only assigned tasks)
+//@route GET /api/tasks/
+//@access Private
+const getTasks = async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+//@desc Get task by ID
+//@route GET/api/tasks/:id
+//@access Private
+
+const getTaskById = async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+//@desc Create a new task (Admin Only)
+//@route POST /api/tasks/
+//@access private (Admin)
+const createTask = async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      priority,
+      dueDate,
+      assignedTo,
+      attachments,
+      todoCheckList
+    } = req.body;
+
+    if (!Array.isArray(assignedTo)) {
+      return res
+        .status(400)
+        .json({ message: "assignedTo must be an array of user IDs" });
+    }
+    const task = await Task.create({
+      title,
+      description,
+      priority,
+      dueDate,
+      assignedTo,
+      createdBy: req.user._id,
+      todoCheckList,
+      attachments,
+    });
+    res.status(201).json({ message: "Task created successfully", task });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+//@desc Update task details 
+//@route PUT/api/tasks/:id
+//@access Private
+const updateTask = async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
+
+//@desc Delete a task (Admin only)
+// @route Delete/api/tasks/:id
+//@access Private (Admin)
+
+const deleteTask = async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+//@desc Update task status
+//@route PUT/api/tasks/:id/status
+//@access Private
+const updateTaskStatus = async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
+
+//@desc Update task checklist 
+//@route PUT/api/tasks/:id/todo
+//@access Private
+const updateTaskChecklist = async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+//@desc Dashboard Data (Admin only)
+//@route GET /api/tasks/dashboard-data
+//@acess Private
+const getDashboardData = async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+//desc Dashboard Data(User-specific)
+//@route GET /api/tasks/user-dashboard-data
+//@access Private Acess
+const getUserDashboardData = async (req, res) => {
+  try {
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+module.exports = {
+  getTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+  updateTaskStatus,
+  updateTaskChecklist,
+  getDashboardData,
+  getUserDashboardData
+};
